ce5711dd47dc6782f49a4596b92576ee3803bbb0
Dino Kotaranin <dino.kotaranin@gmail.com>
2023-01-30 15:04:51 +0100

    api request moved to other component

--- src/App.tsx
+++ src/App.tsx
@@ -10,110 +10,31 @@ import "./App.scss";
 
 import { signal } from "@preact/signals-react";
 
-interface categoryArticles {
-  //totalResults: number;
-  //category?: string; //!
-  articles: {
-    source: { id: string; name: string };
-    author: string;
-    title: string;
-    description: string;
-    url: string;
-    ulrToImage: string;
-    publishedAt: string;
-    content: string;
-    category?: string;
-  }[];
+interface Article {
+  source: { id: string; name: string };
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  ulrToImage: string;
+  publishedAt: string;
+  content: string;
+  category?: string;
 }
 
-interface allArticles extends Array<categoryArticles> {}
-
-const allCategories = [
-  "business",
-  "entertainment",
-  //"general",
-  // "health",
-  // "science",
-  // "sports",
-  // "technology",
-];
 const categorySignal = signal("home");
-const allNews = signal<{}[]>([]); //<allArticles>
-const categoryNews = signal<CategoryResponseAPI>({
-  status: "",
-  totalResults: 0,
-  // category: "",
-  articles: [
-    {
-      source: { id: "", name: "" },
-      author: "",
-      title: "",
-      description: "",
-      url: "",
-      ulrToImage: "",
-      publishedAt: "",
-      content: "",
-    },
-  ],
-});
 
 function App() {
   //const [news, setNews] = useState<ResponseAPI[]>([]);
-  const [allNewsState, setAllNewsState] = useState<{}[]>([]); // <allArticles>
-
-  const getAllArticles = () => {
-    let newsArr: {}[] = [];
-    let newArt = {};
-    allCategories.forEach((category) => {
-      getCategoryNews(category).then((data) => {
-        data.articles.forEach((article) => {
-          newArt = { category: category, ...article };
-          newsArr.push(newArt);
-          //setAllNewsState((old) => [...old, newArt]);
-        });
-      });
-      console.log(category);
-    });
-    setAllNewsState(newsArr);
-    allNews.value = allNewsState;
-  };
-
-  /*
-  //when <React.StrictMode> is there so App gets 2xrender
-  const getAllArticles = () => {
-    let allOfAll: {}[] = [{}];
-    allCategories.forEach((category) =>
-      getCategoryNews(category).then((data) => {
-        data.articles.map((article) => {
-          //let newArt = { category: category, ...article };
-          allOfAll.push({ category: category, ...article });
-          setAllNewsState(new Set(allOfAll));
-        });
-      })
-    );
-  };
-  */
-
-  useEffect(() => {
-    console.log("start");
-    getAllArticles();
-    //allNews.value = allNewsState;
-  }, []);
-
-  function print() {
-    console.log("allNews:", allNews.value);
-    console.log("allNewsState", allNewsState);
-  }
 
   return (
     <div className="App">
       <Header />
       <SearchComponent />
       <hr className="HorizontalLine"></hr>
-      <button onClick={print}>print</button>
       <div className="MainContent">
         <SideMenu categorySignal={categorySignal} />
-        {/* <NewsSection allNews={allNews} /> */}
+        <NewsSection />
       </div>
     </div>
   );
--- src/components/NewsSection/NewsSection.tsx
+++ src/components/NewsSection/NewsSection.tsx
@@ -1,27 +1,91 @@
 import NewsCard from "./NewsCard/NewsCard";
 import LatestNewsCard from "./LatestNewsCard/LatestNewsCard";
+import { useState, useEffect } from "react";
 
 import { GeneralResponseAPI, CategoryResponseAPI } from "../../api/api";
+import { getCategoryNews } from "../../utils/getNews";
+import { signal } from "@preact/signals-react";
 
-/*{allNews.value.map((news, index) => (  ))}*/
+const allCategories = [
+  "business",
+  "entertainment",
+  //"general",
+  // "health",
+  // "science",
+  // "sports",
+  // "technology",
+];
 
-function NewsSection({ allNews }: { allNews: any[any] }) {
-  //{ allNews: { value: CategoryResponseAPI } }
+interface Article {
+  source: { id: string; name: string };
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  ulrToImage: string;
+  publishedAt: string;
+  content: string;
+  category?: string;
+}
+const allNews = signal<Article[]>([]); //<allArticles>
+
+function NewsSection() {
+  const [news, setNews] = useState<Article[]>([]);
+  const [news2, setNews2] = useState<Article[]>([]);
+
+  useEffect(() => {
+    console.log("starting...");
+    let help = getAllArticles();
+    console.log("help:", help);
+    setNews(help);
+    allNews.value = help;
+    console.log("done");
+    // setNewsFun();
+  }, []);
+
+  useEffect(() => {
+    console.log("useEffect");
+    allNews.value = news;
+    console.log("__news:", news);
+    // setNewsFun();
+  }, [news, setNews]);
+
+  const getAllArticles = () => {
+    let newsArr: Article[] = [];
+    let newArt: Article;
+    allCategories.forEach((category) => {
+      getCategoryNews(category).then((data) => {
+        data.articles.forEach((article) => {
+          newArt = { category: category, ...article };
+          newsArr.push(newArt);
+        });
+      });
+    });
+    return newsArr;
+  };
+
+  const setNewsFun = () => {
+    setNews2(news);
+    console.log("News set");
+  };
+
   return (
     <div className="NewsSection">
       <div className="Title">News</div>
+      <button onClick={setNewsFun}>Set News</button>
       <div className="NewsContainer">
         <LatestNewsCard />
-        {allNews.value.articles.map((news, index) => (
+        {news2.map((news, index) => (
           <NewsCard
             key={index}
             order={index}
             category={news.category || ""}
             newsAuthor={news.author || "unknown"}
             newsTitle={news.description}
-            imgUrl="https://sportshub.cbsistatic.com/i/r/2023/01/25/02feadf9-e536-4511-9db5-c971cb086063/thumbnail/1200x675/58f27bf4b4e8ef11d3d8fd60d796e612/beltran-getty-1.png"
+            imgUrl={news.ulrToImage} // //"https://image.cnbcfm.com/api/v1/image/107185642-1675061950003-gettyimages-1192715579-AFP_1NK8BB.jpeg?v=1675062646&w=1920&h=1080"
           />
         ))}
+
         {/*      <NewsCard
           order={2}
           category="tech"
