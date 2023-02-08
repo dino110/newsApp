ec3e302039f3346d26e47e99d78a8f2bfd56b836
Dino Kotaranin <dino.kotaranin@gmail.com>
2023-02-08 12:44:38 +0100

    extra lines removed and search functionality enabled

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,24 @@
-import { useState, useEffect } from "react";
 import Header from "./components/Header/Header";
 import SearchComponent from "./components/SearchComponent/SearchComponent";
 import SideMenu from "./components/SideMenu/SideMenu";
 import NewsSection from "./components/NewsSection/NewsSection";
 
-import { GeneralResponseAPI, CategoryResponseAPI } from "./api/api";
-import { getCategoryNews, getGeneralNews } from "./utils/getNews";
 import "./App.scss";
 
 import { signal } from "@preact/signals-react";
 
-interface Article {
-  source: { id: string; name: string };
-  author: string;
-  title: string;
-  description: string;
-  url: string;
-  ulrToImage: string;
-  publishedAt: string;
-  content: string;
-  category?: string;
-}
-
 const categorySignal = signal("home");
+const searchInput = signal("");
 
 function App() {
-  //const [news, setNews] = useState<ResponseAPI[]>([]);
-
   return (
     <div className="App">
       <Header />
-      <SearchComponent />
+      <SearchComponent searchInput={searchInput} />
       <hr className="HorizontalLine"></hr>
       <div className="MainContent">
         <SideMenu categorySignal={categorySignal} />
-        <NewsSection />
+        <NewsSection searchInput={searchInput} />
       </div>
     </div>
   );
