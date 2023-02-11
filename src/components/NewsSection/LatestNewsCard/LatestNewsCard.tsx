8680f589129c898fb0767e41dad65b5cccd70d19
Dino Kotaranin <dino.kotaranin@gmail.com>
2023-02-11 18:16:45 +0100

    latestNews card functionality added

--- src/components/NewsSection/LatestNewsCard/LatestNewsCard.tsx
+++ src/components/NewsSection/LatestNewsCard/LatestNewsCard.tsx
@@ -1,40 +1,7 @@
 import LatesNewsIcon from "../../../assets/LatestNewsIcon.svg?inline";
-//import LatesNewsIcon from "../../../assets/Search.svg?inline";
+import { Article } from "../NewsSection";
 
-type latestNews = { time: string; title: string };
-
-const latestNewsList: latestNews[] = [
-  {
-    time: "14:30",
-    title: "How To Write Better Advertising Copy",
-  },
-  {
-    time: "14:30",
-    title: "6 Powerful Tips To Creating Testimonials That Sell Your Products",
-  },
-  {
-    time: "14:30",
-    title: "5 Reasons To Choose A Notebook Over A Computer Desktop",
-  },
-  {
-    time: "14:30",
-    title: "6 Powerful Tips To Creating Testimonials That Sell Your Products",
-  },
-  {
-    time: "14:30",
-    title: "Use Your Reset Button",
-  },
-  {
-    time: "14:30",
-    title: "Cdc Issues Health Alert Notice For Travelers To Usa From Hon",
-  },
-  {
-    time: "14:30",
-    title: "Use Your Reset Button",
-  },
-];
-
-function LatestNewsCard() {
+function LatestNewsCard({ allNews }: { allNews: { value: Article[] } }) {
   return (
     <div className="LatestNewsCard">
       <div className="TitleSection">
@@ -42,9 +9,11 @@ function LatestNewsCard() {
         <p>Latest News</p>
       </div>
       <div className="LatestNewsSection">
-        {latestNewsList.map((news, index) => (
+        {allNews.value.map((news, index) => (
           <div className="SingleNews" key={index}>
-            <div className="NewsTime">{news.time}</div>
+            <div className="NewsTime">
+              {news.publishedAt.split("T")[1].slice(0, 5)}
+            </div>
             <div className="NewsTitle">{news.title}</div>
           </div>
         ))}
