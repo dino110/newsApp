328ca330d81a92b0420d639e8af600569c73c0f5
Dino Kotaranin <dino.kotaranin@gmail.com>
2023-02-15 13:36:47 +0100

    extra lines removed

--- src/components/NewsSection/NewsSection.tsx
+++ src/components/NewsSection/NewsSection.tsx
@@ -91,58 +91,6 @@ function NewsSection({
               imgUrl={news.urlToImage || imageNotAvailable}
             />
           ))}
-
-        {/*      <NewsCard
-          order={2}
-          category="tech"
-          newsAuthor="Caroline Parsons"
-          newsTitle="2"
-          imgUrl="https://sportshub.cbsistatic.com/i/r/2023/01/25/02feadf9-e536-4511-9db5-c971cb086063/thumbnail/1200x675/58f27bf4b4e8ef11d3d8fd60d796e612/beltran-getty-1.png"
-        />
-
-        <NewsCard
-          order={3}
-          category="tech"
-          newsAuthor="Caroline Parsons"
-          newsTitle="3"
-          imgUrl="https://sportshub.cbsistatic.com/i/r/2023/01/25/02feadf9-e536-4511-9db5-c971cb086063/thumbnail/1200x675/58f27bf4b4e8ef11d3d8fd60d796e612/beltran-getty-1.png"
-        />
-        <NewsCard
-          order={4}
-          category="tech"
-          newsAuthor="Caroline Parsons"
-          newsTitle="4"
-          imgUrl="https://sportshub.cbsistatic.com/i/r/2023/01/25/02feadf9-e536-4511-9db5-c971cb086063/thumbnail/1200x675/58f27bf4b4e8ef11d3d8fd60d796e612/beltran-getty-1.png"
-        />
-        <NewsCard
-          order={6}
-          category="tech"
-          newsAuthor="Caroline Parsons"
-          newsTitle="5"
-          imgUrl="https://sportshub.cbsistatic.com/i/r/2023/01/25/02feadf9-e536-4511-9db5-c971cb086063/thumbnail/1200x675/58f27bf4b4e8ef11d3d8fd60d796e612/beltran-getty-1.png"
-        />
-        <NewsCard
-          order={7}
-          category="tech"
-          newsAuthor="Caroline Parsons"
-          newsTitle="6"
-          imgUrl="https://sportshub.cbsistatic.com/i/r/2023/01/25/02feadf9-e536-4511-9db5-c971cb086063/thumbnail/1200x675/58f27bf4b4e8ef11d3d8fd60d796e612/beltran-getty-1.png"
-        />
-
-        <NewsCard
-          order={8}
-          category="tech"
-          newsAuthor="Caroline Parsons"
-          newsTitle="7"
-          imgUrl="https://sportshub.cbsistatic.com/i/r/2023/01/25/02feadf9-e536-4511-9db5-c971cb086063/thumbnail/1200x675/58f27bf4b4e8ef11d3d8fd60d796e612/beltran-getty-1.png"
-        />
-        <NewsCard
-          order={9}
-          category="tech"
-          newsAuthor="Caroline Parsons"
-          newsTitle="8"
-          imgUrl="https://sportshub.cbsistatic.com/i/r/2023/01/25/02feadf9-e536-4511-9db5-c971cb086063/thumbnail/1200x675/58f27bf4b4e8ef11d3d8fd60d796e612/beltran-getty-1.png"
-        /> */}
       </div>
     </div>
   );
